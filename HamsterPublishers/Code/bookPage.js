7f15bcbb9071adc23f3e66b78fe1f190670d7dcd
WizardGit <slocumkaiser@gmail.com>
2022-07-09 16:22:25 -0600

    Final edits for hamster publishers

--- HamsterPublishers/Code/bookPage.js
+++ HamsterPublishers/Code/bookPage.js
@@ -1,58 +1,58 @@
 /*
-    Filename: formsubmit.js
+    Filename: bookPage.js
     Author: Kaiser Slocum
-    Date (Last Edited): 12/29/2021
+    Date (Last Edited): 7/9/2022
 
-   Purpose: The purpose of this program is to display the new website, once the correct password has been entered.
+   Purpose: The purpose of this program is to display the pdf book on the webpage
 */
 
 function httass()
 {    
     document.getElementById("BookDisplay").src = "../Books/HowToTrainASpeedStinger.pdf#toolbar=0";  
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>How To Train A Speed Stinger</ins>!";
+    document.getElementById("title").innerHTML = "<ins>How To Train A Speed Stinger</ins>";
 }
 function httad()
 {    
     document.getElementById("BookDisplay").src = "../Books/HowToTrainADreadstrider.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>How To Train A Dreadstrider</ins>!";
+    document.getElementById("title").innerHTML = "<ins>How To Train A Dreadstrider</ins>";
 }
 function httads()
 {    
     document.getElementById("BookDisplay").src = "../Books/HowToTrainADeadlyScuttleclaw.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>How To Train A Deadly Scuttleclaw</ins>!";
+    document.getElementById("title").innerHTML = "<ins>How To Train A Deadly Scuttleclaw</ins>";
 }
 function httabd()
 {    
     document.getElementById("BookDisplay").src = "../Books/HowToTrainABewilderdeath.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>How To Train A Bewilderdeath</ins>!";
+    document.getElementById("title").innerHTML = "<ins>How To Train A Bewilderdeath</ins>";
 }
 function forceless()
 {    
     document.getElementById("BookDisplay").src = "../Books/Forceless.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>Forceless</ins>!";
+    document.getElementById("title").innerHTML = "<ins>Forceless</ins>";
 }
 function forceful()
 {    
     document.getElementById("BookDisplay").src = "../Books/Forceful.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>Forceful</ins>!";
+    document.getElementById("title").innerHTML = "<ins>Forceful</ins>";
 }
 function sst()
 {    
     document.getElementById("BookDisplay").src = "../Books/TheSwordTheScepterAndTheTraitor.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>The Sword, The Scepter, and The Traitor</ins>!";
+    document.getElementById("title").innerHTML = "<ins>The Sword, The Scepter, and The Traitor</ins>";
 }
 function thereturn()
 {    
     document.getElementById("BookDisplay").src = "../Books/TheReturn.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>The Return: Elven Decimation</ins>!";
+    document.getElementById("title").innerHTML = "<ins>The Return: Elven Decimation</ins>";
 }
 function nofailure()
 {    
     document.getElementById("BookDisplay").src = "../Books/FailureIsNotAnOption.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>Failure Is Not An Option</ins>!";
+    document.getElementById("title").innerHTML = "<ins>Failure Is Not An Option</ins>";
 }
 function wumpworld()
 {    
     document.getElementById("BookDisplay").src = "../Books/wumpworld.pdf#toolbar=0";
-    document.getElementById("title").innerHTML = "Chapter 1 <br> from <ins>Wump World: Redone</ins>!";
+    document.getElementById("title").innerHTML = "<ins>Wump World: Redone</ins>";
 }
