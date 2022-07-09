7108a3e9dddb1920d278b47dddaaba688902148f
WizardGit <slocumkaiser@gmail.com>
2022-07-09 15:37:58 -0600

    Website Updated

---
+++ HamsterPublishers/Code/bookPage.js
@@ -0,0 +1,64 @@
+/*
+    Filename: formsubmit.js
+    Author: Kaiser Slocum
+    Date (Last Edited): 12/29/2021
+
+   Purpose: The purpose of this program is to display the new website, once the correct password has been entered.
+*/
+
+function httass()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/HowToTrainASpeedStinger.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>How To Train A Speed Stinger</ins>!";
+}
+function httad()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/HowToTrainADreadstrider.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>How To Train A Dreadstrider</ins>!";
+}
+function httads()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/HowToTrainADeadlyScuttleclaw.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>How To Train A Deadly Scuttleclaw</ins>!";
+}
+function forceless()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/Forceless.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>Forceless</ins>!";
+}
+function forceful()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/Forceful.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>Forceful</ins>!";
+}
+function sst()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/TheSwordTheScepterAndTheTraitor.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>The Sword, The Scepter, and The Traitor</ins>!";
+}
+function thereturn()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/TheReturn.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>The Return: Elven Decimation</ins>!";
+}
+function nofailure()
+{    
+    var ch = document.getElementById("testy");
+    ch.data = '../Books/FailureIsNotAnOption.htm';
+    ch = document.getElementById("title");
+    ch.innerHTML = "Chapter 1 <br> from <ins>Failure Is Not An Option</ins>!";
+}
